d4f1d04894489a563f7958c8fbfec5c850773e59
Paul Johnston <pjohnston571@hotmail.co.uk>
2019-01-03 13:54:42 -0500

    Switch to Hashrouter so direct urls work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { HashRouter as Router, Route} from "react-router-dom";
 import GlobalNav from './components/GlobalNav';
 import SectionContainer from './components/SectionContainer';
 import Dashboard from './components/Dashboard';
