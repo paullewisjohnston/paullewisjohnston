3993790e96058970734807867e6616598b1dbb8a
Paul Johnston <pjohnston571@hotmail.co.uk>
2019-01-03 12:35:10 -0500

    Updates

---
+++ precache-manifest.a85aecb79a504679a4ee22675a422d6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "85f99ca125b1b3feb14e5a90971d20db",
+    "url": "/static/media/qub.85f99ca1.png"
+  },
+  {
+    "revision": "8c51d047169d79759c88ead8c64135f0",
+    "url": "/static/media/logo.8c51d047.png"
+  },
+  {
+    "revision": "0b0bc47694eb2c2460bbe1682ef3c849",
+    "url": "/static/media/infosys.0b0bc476.png"
+  },
+  {
+    "revision": "12b0e1dba88f2f0722d4b56cf9131f15",
+    "url": "/static/media/cora.12b0e1db.png"
+  },
+  {
+    "revision": "8f268005dcfcefacc1ca303b5fae1e18",
+    "url": "/static/media/cocacola.8f268005.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "816a3358bdedb3223d42",
+    "url": "/static/js/main.816a3358.chunk.js"
+  },
+  {
+    "revision": "c25f06e7ffe75946be7b",
+    "url": "/static/js/1.c25f06e7.chunk.js"
+  },
+  {
+    "revision": "816a3358bdedb3223d42",
+    "url": "/static/css/main.6eec25c0.chunk.css"
+  },
+  {
+    "revision": "cf9761fdcc379c55f9a1aed4e8cd3a41",
+    "url": "/index.html"
+  }
+];
