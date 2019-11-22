427c4121f0a2dddc7446e933a771f3b4324d50c1
Paul Johnston <pjohnston571@hotmail.co.uk>
2019-11-22 23:06:07 +0000

    Updates

---
+++ precache-manifest.fcac078da0de3e452b1b073305847670.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54adb631191c129b45f848ff8bbd7879",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b0e87f675c6b968d58",
+    "url": "/static/css/2.5c5f4c00.chunk.css"
+  },
+  {
+    "revision": "cd3d889039f950deb9e6",
+    "url": "/static/css/main.b1db2eec.chunk.css"
+  },
+  {
+    "revision": "10b0e87f675c6b968d58",
+    "url": "/static/js/2.ab6f8fbb.chunk.js"
+  },
+  {
+    "revision": "cd3d889039f950deb9e6",
+    "url": "/static/js/main.b9f3aab3.chunk.js"
+  },
+  {
+    "revision": "f88b6db59ad731a50065",
+    "url": "/static/js/runtime~main.e8ba1baf.js"
+  },
+  {
+    "revision": "8f268005dcfcefacc1ca303b5fae1e18",
+    "url": "/static/media/cocacola.8f268005.png"
+  },
+  {
+    "revision": "12b0e1dba88f2f0722d4b56cf9131f15",
+    "url": "/static/media/cora.12b0e1db.png"
+  },
+  {
+    "revision": "466caa5938e43bead70f9888d03ccf79",
+    "url": "/static/media/infosys.466caa59.png"
+  },
+  {
+    "revision": "8c51d047169d79759c88ead8c64135f0",
+    "url": "/static/media/logo.8c51d047.png"
+  },
+  {
+    "revision": "85f99ca125b1b3feb14e5a90971d20db",
+    "url": "/static/media/qub.85f99ca1.png"
+  },
+  {
+    "revision": "4d4aa6b495e2abf05ebf805392484fcc",
+    "url": "/static/media/umg.4d4aa6b4.jpg"
+  }
+]);
